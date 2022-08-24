982473018e674a2894de56a6052d8b95ff04894f
karthiBscCs <35680255+karthiBscCs@users.noreply.github.com>
2022-08-24 22:08:34 +0530

    Added files
    I just create this project to convert Essentials v18 trainer to v20.1 trainer because v18 and v19 is most people using so i figure out to convert
    i used .net for this
    this code is not completed some basic steps are completed but i don't know the idea's any idea's or code's are welcome ;)

---
+++ TrainerConverter/TrainerConverter/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace TrainerConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.UpldTrnr = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Trainer.txt:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // UpldTrnr
+            // 
+            this.UpldTrnr.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UpldTrnr.Location = new System.Drawing.Point(149, 34);
+            this.UpldTrnr.Name = "UpldTrnr";
+            this.UpldTrnr.Size = new System.Drawing.Size(150, 33);
+            this.UpldTrnr.TabIndex = 1;
+            this.UpldTrnr.Text = "Upload";
+            this.UpldTrnr.UseVisualStyleBackColor = true;
+            this.UpldTrnr.Click += new System.EventHandler(this.UpldTrnr_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 261);
+            this.Controls.Add(this.UpldTrnr);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button UpldTrnr;
+    }
+}
+
---
+++ TrainerConverter/TrainerConverter/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TrainerConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void UpldTrnr_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Text files | *.txt";
+            dialog.Multiselect = false;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                String path = dialog.FileName;
+                string[] text = System.IO.File.ReadAllLines(path);
+                //i'm writting on D drive id you have C then change this
+                TextWriter txt = new StreamWriter("D:\\CnvTrainer\\trainers.txt");
+                for (int i = 0; i < text.Length; i++)
+                {
+                    string GetTxt = text[i];
+
+                    //Write on txt
+                    if (i == 0)
+                    {
+                        txt.Write(GetTxt);
+                    }
+                    else
+                    {
+                        if (text[i] == "#-------------------------------")
+                        {
+                            txt.Write("\n" + GetTxt);
+                        }
+                        else
+                        {
+                            if (GetTxt == "1" || GetTxt == "2" || GetTxt == "3" || GetTxt == "4" || GetTxt == "5" || GetTxt == "6")
+                            {
+
+                            }
+                            else if (ChkCap(GetTxt) == true)
+                            {
+                                var endIndex = GetTxt.LastIndexOf(',');
+                                if (endIndex != -1)
+                                {
+                                    txt.Write("\n" + "Pokemon =" + GetTxt);
+                                }
+                                else
+                                {
+                                    txt.Write("\n" + "[" + GetTxt + "]");
+                                }
+                            }
+                            else if (ChkCap(GetTxt) == false)
+                            {
+                                txt.Write("\nLoseText = Test");
+                            }
+                        }
+                    }
+                }
+                txt.Close();
+                MessageBox.Show("Converted!!");
+            }
+        }
+
+        public static bool ChkCap(string str1)
+        {
+            return str1 == str1.ToUpper() | str1 == str1.ToLower();
+        }
+    }
+}
